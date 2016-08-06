5d9b03e791c8e6f0f48c4f6de54495bcc53211f2
Y. Yan <dev@ybank.net>
2016-08-06 03:28:47 -0700

    new src old test

--- gson/src/test/java/com/google/gson/JsonPrimitiveTest.java
+++ gson/src/test/java/com/google/gson/JsonPrimitiveTest.java
@@ -229,18 +229,8 @@ public class JsonPrimitiveTest extends TestCase {
   public void testEqualsIntegerAndBigInteger() {
     JsonPrimitive a = new JsonPrimitive(5L);
     JsonPrimitive b = new JsonPrimitive(new BigInteger("18446744073709551621")); // 2^64 + 5
-    assertFalse(a + " equals " + b, a.equals(b));
-
-    a = new JsonPrimitive(new BigInteger("18446744073709551621")); // 2^64 + 5
-    b = new JsonPrimitive(5L);
-    assertFalse(a + " equals " + b, a.equals(b));
-
-    a = new JsonPrimitive(new BigInteger("18446744073709551621")); // 2^64 + 5
-    b = new JsonPrimitive(new BigInteger("18446744073709551620")); // 2^64 + 4
-    assertFalse(a + " equals " + b, a.equals(b));
-
-    a = new JsonPrimitive(new BigInteger("18446744073709551621")); // 2^64 + 5
-    b = new JsonPrimitive(new BigInteger("18446744073709551621")); // 2^64 + 5
+    // Ideally, the following assertion should have failed but the price is too much to pay 
+    // assertFalse(a + " equals " + b, a.equals(b));
     assertTrue(a + " equals " + b, a.equals(b));
   }
 
